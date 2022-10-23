30effec3a78a163b332e2f50a73e9020b71eb73b
abhisheksr01 <abhisheksingh2691@gmail.com>
2022-10-23 16:35:20 +0100

    fix docker build failure missing version file

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -12,18 +12,18 @@ build_image() {
   echo "Start Building Docker Image..."
   TAG=$([ "${CIRCLE_BRANCH}" == "main" ] && echo "0.1.${CIRCLE_BUILD_NUM}" || echo "${CIRCLE_BRANCH}" | sed 's/dependabot\/gradle//g;s/.//')
   echo "New Docker Image Version : ${TAG}"
+  echo "${TAG}" > docker-version.txt
   echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
-  docker build -t "${DOCKER_USER}/${DOCKER_IMAGE_HELLOWORLD}:${TAG}" .
+  docker build -t "${DOCKER_USER}/${DOCKER_IMAGE}:${TAG}" .
 }
 push_image() {
-  TAG=$(cat version/docker-version.txt)
-  echo "print value from test.tx"
-  cat version/test.txt
+  TAG=$(cat docker-version.txt)
+  echo "Will push a docker image with tag : ${TAG}"
   if [ "${CIRCLE_BRANCH}" == "main" ]; then
     echo "Pushing Docker Image latest & ${TAG} ..."
-    docker tag "${DOCKER_USER}/${DOCKER_IMAGE_HELLOWORLD}:${TAG}" "${DOCKER_USER}/${DOCKER_IMAGE_HELLOWORLD}:latest"
-    docker push "${DOCKER_USER}/${DOCKER_IMAGE_HELLOWORLD}:${TAG}"
-    docker push "${DOCKER_USER}/${DOCKER_IMAGE_HELLOWORLD}:latest"
+    docker tag "${DOCKER_USER}/${DOCKER_IMAGE}:${TAG}" "${DOCKER_USER}/${DOCKER_IMAGE}:latest"
+    docker push "${DOCKER_USER}/${DOCKER_IMAGE}:${TAG}"
+    docker push "${DOCKER_USER}/${DOCKER_IMAGE}:latest"
   else
     echo "Skipping pushing docker image for non main branch."
   fi
